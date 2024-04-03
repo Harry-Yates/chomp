2dd945764750f61bfb5340f3adb51efbedf24aad
Harr-Yates <hyates1@gmail.com>
2024-04-03 16:36:20 +0200

    base setup

---
+++ src/pages/company/history.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const history = () => {
+  return <div>this is our history</div>;
+};
+
+export default history;
---
+++ src/pages/contact.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const contact = () => {
+  return <div>hello from the contact</div>;
+};
+
+export default contact;
