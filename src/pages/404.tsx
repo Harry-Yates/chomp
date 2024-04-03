b3d88bfb45d8c9c1cb2723d69659d56a61d05142
Harr-Yates <hyates1@gmail.com>
2024-04-03 18:01:02 +0200

    tidy up

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 
-const NotFoundPage = () => {
-  return (
-    <main>
-      <h1>Page not found</h1>
-      <p>Sorry 😔, we couldn’t find what you were looking for.</p>
-      <a href="/">Go home</a>.
-    </main>
-  );
+const PageNotFound = () => {
+	return (
+		<main>
+			<h1>Page not found</h1>
+			<p>Sorry 😔, we couldn’t find what you were looking for.</p>
+			<a href="/">Go home</a>.
+		</main>
+	);
 };
 
-export default NotFoundPage;
+export default PageNotFound;
