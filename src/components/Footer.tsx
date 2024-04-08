91ec9af9af2e34a7dd5f27f2148c7fb02efe20fc
Harr-Yates <hyates1@gmail.com>
2024-04-08 14:53:20 +0200

    tidy up footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import logo from "../assets/images/logo.png";
+import { Link } from "gatsby";
 
 const Footer = () => {
 	return (
 		<footer className="page-footer">
-			<p>
-				&copy; {new Date().getFullYear()} <span>CHOMP</span>
-			</p>
+			<Link to="/">
+				<img
+					src={logo}
+					alt="Simply Recipes"
+					style={{ height: "2.5rem", width: "2.5rem" }}
+				/>
+			</Link>
 		</footer>
 	);
 };
