843470a8f3efff938bc0e1b593e3f26233b9dbe6
Harr-Yates <hyates1@gmail.com>
2024-05-21 15:33:03 +0200

    added tags

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,14 +1,47 @@
-import React from "react";
-import Layout from "../components/Layout";
+import React from "react"
+import Layout from "../components/Layout"
+import { graphql, Link } from "gatsby"
+import setupTags from "../utils/setupTags"
+import slugify from "slugify"
+import type { RecipesQueryResult } from "../types/Recipe"
+// import SEO from "../components/SEO"
 
-const Tags = () => {
+const Tags = ({ data }: { data: RecipesQueryResult }) => {
+	const recipes = data.allContentfulRecipe.nodes;
+	const newTags = setupTags({ recipes });
+
 	return (
 		<Layout>
-			<div className="about-heading">
-				<h1>Tags</h1>
-			</div>
-		</Layout>
-	);
-};
+			{/* <SEO title="Tags" /> */}
+			<main className="page">
+				<section className="tags-page">
+					{newTags.map((tag) => {
+						const [text, value] = tag
+						const slug = slugify(text, { lower: true })
 
-export default Tags;
+						return (
+							<Link to={`/tags/${slug}`} key={text} className="tag">
+								<h5>{text}</h5>
+								<p>{value} recipe</p>
+							</Link>
+						)
+					})}
+				</section>
+			</main>
+		</Layout>
+	)
+}
+
+export const query = graphql`
+  {
+    allContentfulRecipe {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`
+
+export default Tags
