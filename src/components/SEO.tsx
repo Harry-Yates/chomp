223cd4762b947cba25ca83ad6e54bd2e767e2e13
Harr-Yates <hyates1@gmail.com>
2024-05-22 21:08:07 +0200

    tidy up to seo

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,6 +1,6 @@
-import React from "react"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import { Helmet } from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
 
 const query = graphql`
   {
@@ -11,23 +11,32 @@ const query = graphql`
       }
     }
   }
-`
+`;
 
 interface SEOProps {
-    title: string;
-    description?: string;
+  title: string;
+  description?: string;
 }
 
 const SEO = ({ title, description }: SEOProps) => {
-    const { site } = useStaticQuery(query)
-    const metaDescription = description || site.siteMetadata.description
-    return (
-        <Helmet
-            htmlAttributes={{ lang: "en" }}
-            title={`${title} | ${site.siteMetadata.title}`}
-            meta={[{ name: "description:", content: metaDescription }]}
-        />
-    )
+  const { site } = useStaticQuery(query);
+  const metaDescription = description || site.siteMetadata.description;
+
+  // Function to convert a string to sentence case
+  const toSentenceCase = (str: string) => {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  };
+
+  const formattedSiteTitle = toSentenceCase(site.siteMetadata.title);
+  const formattedTitle = toSentenceCase(title);
+
+  return (
+    <Helmet
+      htmlAttributes={{ lang: "en" }}
+      title={`${formattedTitle} | ${formattedSiteTitle}`}
+      meta={[{ name: "description", content: metaDescription }]}  // Corrected the meta name field
+    />
+  );
 }
 
-export default SEO
+export default SEO;
